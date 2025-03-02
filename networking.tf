94d316c532f2ea3791a68d03f35e8c389c168c22
Erielcio Imamura <erielcio00@gmail.com>
2025-03-02 06:59:35 -0300

    f

--- networking.tf
+++ networking.tf
@@ -1,17 +1,17 @@
-module "vpc" {
-  source          = "./modules/vpc"     # Path to the VPC module
-  cidr_block      = var.cidr_block      # CIDR block for the VPC
-  public_subnets  = var.public_subnets  # Public subnets CIDR
-  private_subnets = var.private_subnets # Private subnets CIDR
-  vpc_name        = var.vpc_name        # VPC name
-  instance_type   = var.instance_type   # EC2 instance type
-  key_name        = var.key_name        # EC2 key pair name
-  tags            = var.tags            # Tags for resources
-  enable_nat      = false
-}
+# module "vpc" {
+#   source          = "./modules/vpc"     # Path to the VPC module
+#   cidr_block      = var.cidr_block      # CIDR block for the VPC
+#   public_subnets  = var.public_subnets  # Public subnets CIDR
+#   private_subnets = var.private_subnets # Private subnets CIDR
+#   vpc_name        = var.vpc_name        # VPC name
+#   instance_type   = var.instance_type   # EC2 instance type
+#   key_name        = var.key_name        # EC2 key pair name
+#   tags            = var.tags            # Tags for resources
+#   enable_nat      = false
+# }
 
-module "sg" {
-  source  = "./modules/security_group"
-  vpc_id  = module.vpc.vpc_id
-  project = var.project
-}
+# module "sg" {
+#   source  = "./modules/security_group"
+#   vpc_id  = module.vpc.vpc_id
+#   project = var.project
+# }
